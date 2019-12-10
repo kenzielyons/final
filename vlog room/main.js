1d3b6274612ca59f5f3b5121b9bc91c06aea2cd3
MacKenzie Lyons <MacKenzie.Lyons@sckans.edu>
2019-12-10 15:26:25 -0600

    created final cite

---
+++ vlog room/main.js
@@ -0,0 +1,8 @@
+const socket = new WebSocket('wss://www.el-evan.com/web_dev_chat_server');
+document.querySelector('#submit').addEventListener('click', () =>{
+    socket.send(JSON.stringify({Name : document.querySelector('#name').value, Message: document.querySelector('#message').value}))
+});
+socket.addEventListener('message' , event => {
+    const message = JSON.parse(event.data);
+    document.querySelector('.chat-stream').innerText += message.Name + ' :: ' + message.Message + '\n'; 
+});
